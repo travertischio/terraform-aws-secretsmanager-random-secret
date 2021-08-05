b781a988727ac0e7788921dcbe4ec7a8972faddb
Traver Tischio <traver.tischio@clarifai.com>
2021-08-05 14:59:21 -0400

    Add back in sensitive secret with proper terraform sensitive value

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
+output "secret" {
+  description = "Generated secret"
+  value       = var.create_secret ? random_password.random_string[0].result : ""
+  sensitive   = true
+}
+
 output "secret_arn" {
   description = "The ARN of the secret"
   value       = var.create_secret ? aws_secretsmanager_secret.secret[0].arn : ""
