5baa90effbd2f583d6a3b4acc984b79a135a5590
Traver Tischio <traver.tischio@clarifai.com>
2021-08-05 14:52:09 -0400

    Remove sensitive secret output

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "secret" {
-  description = "Generated secret"
-  value       = var.create_secret ? random_password.random_string[0].result : ""
-}
-
 output "secret_arn" {
   description = "The ARN of the secret"
   value       = var.create_secret ? aws_secretsmanager_secret.secret[0].arn : ""
